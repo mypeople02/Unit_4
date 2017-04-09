d480dc6b83e934c502c5e1815c0bd3c99dccb6b3
mypeople02 <ammitan@gmail.com>
2017-04-09 15:45:13 -0500

    added status swirl

--- 4.2.1 Calling a Web Service/Web Service Example/Web Service Example/ViewController.swift
+++ 4.2.1 Calling a Web Service/Web Service Example/Web Service Example/ViewController.swift
@@ -13,10 +13,23 @@ class ViewController: UIViewController {
     @IBOutlet var forecastLabel: UILabel!
     
      override func viewDidLoad() {
-        super.viewDidLoad()   
+        super.viewDidLoad()
+        
+        self.forecastLabel.text = ""
+        
+        //instantiate a gray Activity Indicator View
+        let activityIndicatorView = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+        //add the activity to the ViewController's view
+        view.addSubview(activityIndicatorView)
+        //position the Activity Indicator View in the center of the view
+        activityIndicatorView.center = view.center
+        //tell the Activity Indicator View to begin animating
+        activityIndicatorView.startAnimating()
+        
+        
         let manager = AFHTTPSessionManager()
         
-        manager.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=json&units=metric&cnt=1&appid=e6e3904fe791185b6fb072da91901ab1",
+        manager.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=Houston&mode=json&units=metric&cnt=1&appid=e6e3904fe791185b6fb072da91901ab1",
             
             parameters: nil,
             progress: nil,
@@ -33,6 +46,7 @@ class ViewController: UIViewController {
                             if let firstWeatherOfDay = tomorrowsWeather[0] as? [String:AnyObject] {
                                 if let forecast = firstWeatherOfDay["description"] as? String {
                                     self.forecastLabel.text = forecast
+
                                 }
                             }
                         }
@@ -43,8 +57,12 @@ class ViewController: UIViewController {
             }) { (operation:URLSessionDataTask?, error:Error) in
                 print("Error: " + error.localizedDescription)
             }
+    
+            activityIndicatorView.removeFromSuperview()
     }
 
+
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
